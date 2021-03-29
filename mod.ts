326141435b1d2e61041e63ff689fb13755abe6e6
spontoreau <pontoreau.sylvain@gmail.com>
2021-03-29 18:59:11 +0200

    test

--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello World test!", {
+  const response = new Response("Hello World!", {
     headers: { "content-type": "text/plain" },
   });
   event.respondWith(response);
